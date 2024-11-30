914e8e958c5e720e65a3ef64531a345a41ceb50a
貝時計 KAITOKEI <m21148@g.metro-cit.ac.jp>
2024-11-30 15:09:28 +0900

    add: fin?

--- main.cpp
+++ main.cpp
@@ -8,14 +8,43 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
     // グラフデータを読み込み
     vector<Vertex> vtxs = read_graph_data("sample.txt");
 
-    // 記号列を読み込み
+    string log = "Q,sigma,delta,";     // 履歴
+    vector<int> crt_id = {0}, prev_id; // nodeのid (分岐するから、vectorで管理)
+
+    string input = argv[1];           // 入力記号列
+    string accepted_lambda = argv[2]; // 受理条件の文字列
+
     // 記号列をなぞる。遷移状況をcsv形式で記録する
-    // 最後に到達したnodeのlambdaを取得し、出力
+    for (const char &v : input)
+    {
+        prev_id = crt_id;
+        crt_id = vector<int>(0);
+
+        for (const int &i : prev_id)
+        {
+            for (const int &t : vtxs[i].to[v])
+            {
+                crt_id.push_back(t);
+                log += "q" + to_string(i) + "," + v + "," + "q" + to_string(t) + "\n";
+            }
+        }
+    }
+
+    // 最後に到達したnodeのlambdaを取得し、受理したかどうかを出力
+    for (const int &i : crt_id)
+    {
+        if (vtxs[i].lambda == accepted_lambda)
+        {
+            cout << "Accepted!!" << endl;
+            return 0;
+        }
+    }
+    cout << "Rejected!!" << endl;
 
     return 0;
 }
