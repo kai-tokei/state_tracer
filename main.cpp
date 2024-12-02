6e28b2119b673c9b2ae3d3c19ac076be7e49e8b7
貝時計 KAITOKEI <m21148@g.metro-cit.ac.jp>
2024-12-02 21:18:55 +0900

    add: accept checker

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <iostream>
 #include <filesystem>
+#include <math.h>
 
 #include "vertex.cpp"
 #include "utils.cpp"
@@ -10,7 +11,18 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-// 入力記号列を元にfaをトレースし、受理されるかどうかを判定
+string binary(int bina)
+{
+    int ans = 0;
+    for (int i = 0; bina > 0; i++)
+    {
+        ans = ans + (bina % 2) * pow(10, i);
+        bina = bina / 2;
+    }
+    return to_string(ans);
+}
+
+// トレース結果から、受理されるかどうかを判定
 bool is_accepted(vector<Vertex> &vtxs, vector<int> &crt_id, string &accepted_lambda)
 {
     bool is_accepted = false;
@@ -22,31 +34,11 @@ bool is_accepted(vector<Vertex> &vtxs, vector<int> &crt_id, string &accepted_lam
     return is_accepted;
 }
 
-int main(int argc, char *argv[])
+// 頂点をトレースする
+string trace(vector<Vertex> &vtxs, string input, vector<int> &crt_id)
 {
-    if (argc < 2)
-    {
-        cerr << "Error: Invalid Augmunts!!" << endl;
-        return 0;
-    }
-
-    string input = argv[1];       // 入力記号列
-    string accepted_lambda = "1"; // 受理条件の文字列
-    string graph_file_path = "sub_graph.txt";
-    if (argc > 2)
-    {
-        graph_file_path = argv[2];
-    }
-    if (argc > 3)
-    {
-        accepted_lambda = argv[3];
-    }
-
-    // グラフデータを読み込み
-    vector<Vertex> vtxs = read_graph_data(graph_file_path);
-
-    string log = "Q,sigma,delta,lambda,\n"; // 履歴
-    vector<int> crt_id = {0}, prev_id;      // nodeのid (分岐するから、vectorで管理)
+    string log = "";
+    vector<int> prev_id;
 
     // 記号列をなぞる。遷移状況をcsv形式で記録する
     for (const char &v : input)
@@ -71,8 +63,49 @@ int main(int argc, char *argv[])
         log += ",,,,\n";
     }
 
-    // 最後に到達したnodeのlambdaを取得し、受理したかどうかを出力
-    cout << (is_accepted(vtxs, crt_id, accepted_lambda) ? "Accepted!!" : "Rejected!!") << endl;
+    return log;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        cerr << "Error: Invalid Augmunts!!, (path) (input string) [lambda]" << endl;
+        return 0;
+    }
+    string graph_file_path = argv[1]; // 読み込むファイルのpath
+    string input = "-1";              // 入力記号列
+    string accepted_lambda = "1";     // 受理条件の文字列
+    if (argc > 2)
+    {
+        input = argv[2];
+    }
+    if (argc > 3)
+    {
+        accepted_lambda = argv[3];
+    }
+
+    vector<Vertex> vtxs = read_graph_data(graph_file_path); // グラフデータを読み込み
+    string log = "Q,sigma,delta,lambda,\n";                 // 履歴
+
+    if (input == "-1")
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            vector<int> crt_id = {0};             // nodeのid (分岐するから、vectorで管理)
+            log = trace(vtxs, binary(i), crt_id); // グラフのトレース
+            if (is_accepted(vtxs, crt_id, accepted_lambda))
+            {
+                cout << i << ": " << binary(i) << endl;
+            }
+        }
+    }
+    else
+    {
+        vector<int> crt_id = {0};                                                                   // nodeのid (分岐するから、vectorで管理)
+        log = trace(vtxs, input, crt_id);                                                           // グラフのトレース
+        cout << (is_accepted(vtxs, crt_id, accepted_lambda) ? "Accepted!!" : "Rejected!!") << endl; // 受理したかどうかを
+    }
 
     // 遷移表を書き出し
     fs::create_directories("tables");
